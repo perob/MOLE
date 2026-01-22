2b5eaef2d415794105018184b8f333fd0bcf09db
perob <pbrbora@hotmail.com>
2026-01-22 17:39:13 +0100

    Added vbr test

--- examples/Any2Mp4/Test/TestRun.bat
+++ examples/Any2Mp4/Test/TestRun.bat
@@ -8,8 +8,10 @@ set any2mp4="..\Builds\VisualStudio2022\x64\Debug\ConsoleApp\Any2Mp4.exe"
 %any2mp4% --mp2 %input% sample.mp2
 %any2mp4% --mp1 %input% sample.mp1
 %any2mp4% --flac %input% sample.flac.mp4
-%any2mp4% --wma8 %input% sample.wma8.wma
-%any2mp4% --wma9 %input% sample.wma9.wma
+%any2mp4% --wma8 %input% sample.wma8.cbr.wma
+%any2mp4% --wma8 --vbrq 90 %input% sample.wma8.vbr.wma
+%any2mp4% --wma9 %input% sample.wma9.cbr.wma
+%any2mp4% --wma9 --vbrq 90 %input% sample.wma9.vbr.wma
 %any2mp4% --wmal %input% sample.wmaL.wma
 %any2mp4% --wmsp %voice% sample.wmsp.wma
 rem %any2mp4% --ac3 %input% sample.ac3
