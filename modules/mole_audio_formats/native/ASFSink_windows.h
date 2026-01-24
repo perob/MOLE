32efc457588c20c7d1e92535cd8c81bf6dd4d21d
perob <pbrbora@hotmail.com>
2026-01-24 12:19:21 +0100

    Fixed pointer usage

--- modules/mole_audio_formats/native/ASFSink_windows.h
+++ modules/mole_audio_formats/native/ASFSink_windows.h
@@ -103,7 +103,7 @@ namespace mole {
                 }
                 else
                 {
-                    transform.AddRef();
+                    transform->AddRef();
                 }
             }
 
